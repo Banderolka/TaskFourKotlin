fecb9b3334f7c6abfca45f494f925d8574d4c8ee
Banderolka <bothisava@gmail.com>
2023-06-12 20:51:36 +0300

    Задание № 19

--- src/Aircraft.kt
+++ src/Aircraft.kt
@@ -1,7 +1,13 @@
 
-class Aircraft (val aircraftNumber: Int,  val maximumFlightRange: Int, val tankCapacity: Int) {
+
+ open class Aircraft (val aircraftNumber: Int, val maximumFlightRange: Int, val tankCapacity: Int) {
+
 
     var fuelConsumption: Int = 100
         get() = tankCapacity/maximumFlightRange * 100
 
 }
+
+ class Boeing747(val capacity: Int, aircraftNumber: Int, maximumFlightRange: Int,tankCapacity: Int): Aircraft (aircraftNumber, maximumFlightRange, tankCapacity){
+
+ }
